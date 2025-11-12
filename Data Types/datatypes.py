29a0e8619d338fb233a13d1ba13a002fe27de33f
Vedasri-Git <vedasri.atla@gmail.com>
2025-11-12 16:53:31 +0530

    Add files via upload

---
+++ Data Types/datatypes.py
@@ -0,0 +1,8 @@
+name= "vedasri"
+a=54
+b=8.2
+status=True
+print(type(name))
+print(type(a))
+print(type(b))
+print(type(status))
---
+++ Data Types/typecasting.py
@@ -0,0 +1,29 @@
+
+
+
+                          
+name = "Vedasri"
+
+age = 9
+
+weight = 21.5
+
+is_Student = True
+
+# Convert integer to string
+
+age = str(age)
+
+print(type(age))
+
+# covert float to integer
+
+weight = int(weight)
+
+print(type (weight))
+
+# covert integer to float
+
+age = float(age)
+
+print(type(age))
