3912d75e16afb2176dc135c6294e91e44e47a733
Vedasri-Git <vedasri.atla@gmail.com>
2025-12-16 16:58:01 +0530

    Add files via upload

---
+++ Python Operators 3/grade.py
@@ -0,0 +1,34 @@
+
+eng = 91
+math = 98
+idp = 84
+tel = 96
+
+average=eng+ math+idp+tel/5
+print(average)
+
+if (average >= 91) & (average <= 100):
+
+    print("Grade A")
+
+elif (average >= 81) & (average <= 90):
+
+    print("Grade B")
+
+elif (average >= 71) & (average <= 80):
+
+    print("Grade C")
+
+elif (average >= 61) & (average <= 70):
+
+    print("Grade D")
+
+elif (average >= 51) & (average <= 60):
+
+    print("Grade E")
+
+elif (average >= 0) & (average <= 50):
+
+    print("Grade F")
+
+    
